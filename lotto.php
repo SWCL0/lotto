7e225f8466b26b27f124e0f883fcea17af4d7307
SWCL0 <stan.w.c@icloud.com>
2024-02-09 08:20:35 +0100

    Create lotto.php
    Aanmaak bestand
    Opvraag gegevens gebruiken en opslaan in array

---
+++ lotto.php
@@ -0,0 +1,21 @@
+<?php
+
+$invoer = array();
+
+echo "Geef je lotto getallen (getallen moeten tussen 1 en 42 liggen)";
+echo "Geef lotto nummer 1:" . PHP_EOL;
+$invoer = readline();
+echo "Geef lotto nummer 2:" . PHP_EOL;
+$invoer = readline();
+echo "Geef lotto nummer 3:" . PHP_EOL;
+$invoer = readline();
+echo "Geef lotto nummer 4:" . PHP_EOL;
+$invoer = readline();
+echo "Geef lotto nummer 5:" . PHP_EOL;
+$invoer = readline();
+echo "Geef lotto nummer 6:" . PHP_EOL;
+$invoer = readline();
+
+var_dump($invoer);
+
+?>
