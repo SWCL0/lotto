15306dd7c1676514d2b4c50fffd24d8028c586a9
SWCL0 <stan.w.c@icloud.com>
2024-02-09 08:37:59 +0100

    Update lotto.php
    opvraag gegevens -> worden opgeslagen in array. worden weer gegeven aan gebruiker. Array voor lotto getalen word gevuld en gesorteerd. Die word weergegeven aan gebruiker.

--- lotto.php
+++ lotto.php
@@ -1,21 +1,36 @@
 <?php
 
 $invoer = array();
+$getLotto = array();
 
-echo "Geef je lotto getallen (getallen moeten tussen 1 en 42 liggen)";
+echo "Geef je lotto getallen (getallen moeten tussen 1 en 42 liggen)" . PHP_EOL;
 echo "Geef lotto nummer 1:" . PHP_EOL;
-$invoer = readline();
+$invoer[] = readline();
 echo "Geef lotto nummer 2:" . PHP_EOL;
-$invoer = readline();
+$invoer[] = readline();
 echo "Geef lotto nummer 3:" . PHP_EOL;
-$invoer = readline();
+$invoer[] = readline();
 echo "Geef lotto nummer 4:" . PHP_EOL;
-$invoer = readline();
+$invoer[] = readline();
 echo "Geef lotto nummer 5:" . PHP_EOL;
-$invoer = readline();
+$invoer[] = readline();
 echo "Geef lotto nummer 6:" . PHP_EOL;
-$invoer = readline();
+$invoer[] = readline();
 
-var_dump($invoer);
+echo "Je gekozen cijfers zijn: ";
+foreach ($invoer as $getal) {
+    echo $getal . " ";
+}
+
+for ($i = 0; $i < 6; $i++) {
+    $getLotto[] = rand(1, 42);
+}
+sort($getLotto);
+
+echo "De trekking cijfers zijn: ";
+foreach ($getLotto as $outLotto) {
+    echo $outLotto . " ";
+}
+
 
 ?>
